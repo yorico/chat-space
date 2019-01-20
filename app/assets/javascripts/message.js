2dec6c072f44209f6bfbf0ed099d571d87bc51a9
yorico <yori@yorikonoMacBook-puro.local>
2019-01-20 14:49:01 +0900

    rev_message_html,js,jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,27 @@
-$(document).on('turbolinks:load', function() {
-  function buildHTML(message){
-    image = (message.image === null) ? "" : `<img src="${message.image}" class="lower-message__image">`
+$(function(){
+  function buildHTML(content){
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                      ${message.user_name}
+                      ${content.user_name}
                     </div>
                     <div class="upper-message__date">
-                      ${message.created_at}
+                      ${content.created_at}
                     </div>
                   </div>
-                  <div class="lower-message">
+                  <div class="lower-meesage">
                     <p class="lower-message__content">
-                      ${message.content}
+                      ${content.content}
                     </p>
                   </div>
-                  ${image}`
+                </div>`
     return html;
   }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -34,8 +33,7 @@ $(document).on('turbolinks:load', function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('#new_message')[0].reset()
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.textbox').val('')
     })
     .fail(function(){
       alert('error');
